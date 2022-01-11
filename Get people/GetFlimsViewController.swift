3ec2dc0e6e88cd0f8512daeefe7db27c7bdefb37
lod <kholodcor@gmail.com>
2022-01-11 12:36:25 -0800

    update

--- Get people/GetFlimsViewController.swift
+++ Get people/GetFlimsViewController.swift
@@ -15,44 +15,26 @@ class GetFlimsViewController: UIViewController {
 
         filmTable.dataSource = self
     
-                // specify the url that we will be sending the GET request to
-                let url = URL(string: "https://swapi.dev/api/films/?format=json")
-                // create a URLSession to handle the request tasks
-                let session = URLSession.shared
-                // create a "data task" to make the request and run completion handler
-                let task = session.dataTask(with: url!, completionHandler: {
-                    // see: Swift closure expression syntax
-                    data, response, error in
-                    // data -> JSON data, response -> headers and other meta-information, error-> if one occurred
-                    // "do-try-catch" blocks execute a try statement and then use the catch statement for errors
-                    do {
-                        // try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
-                        if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
-                                        if let results = jsonResult["results"] {
-                                            // coercing the results object as an NSArray and then storing that in resultsArray
-                                            let resultsArray = results as! NSArray
-                                            // now we can run NSArray methods like count and firstObject
-                                            for result in resultsArray {
-                                                if let data = result as? NSDictionary {
-                                                    if let result = data["title"] {
-                                                        let name = result as! String
-                                                        self.filmsList.append(name)
-                                                    }
-                                                }
-                                            }
-                                            DispatchQueue.main.async {
-                                                self.filmTable.reloadData()
-                                            }
-                                        }
-                                    }
-                                } catch {
-                                    print(error)
-                                }
-                            })
-                        // execute the task and then wait for the response
-                // to run the completion handler. This is async!
-                task.resume()
-        }
+        FlimsModel.getAllFilms(completionHandler: { // passing what becomes "completionHandler" in the 'getAllPeople' function definition in StarWarsModel.swift
+            data, response, error in
+                do {
+                    // Try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
+                    if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                        if let results = jsonResult["results"] as? NSArray {
+                            for person in results {
+                                let personDict = person as! NSDictionary
+                                self.filmsList.append(personDict["title"]! as! String)
+                            }
+                        }
+                    }
+                    DispatchQueue.main.async {
+                        self.filmTable.reloadData()
+                    }
+                } catch {
+                    print("Something went wrong")
+                }
+        })
+    }
     
     }
 
--- Get people/GetPeopleViewController.swift
+++ Get people/GetPeopleViewController.swift
@@ -13,47 +13,34 @@ class GetPeopleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         pTable.dataSource = self
-    
-                // specify the url that we will be sending the GET request to
-                let url = URL(string: "https://swapi.dev/api/people/?format=json")
-                // create a URLSession to handle the request tasks
-                let session = URLSession.shared
-                // create a "data task" to make the request and run completion handler
-                let task = session.dataTask(with: url!, completionHandler: {
-                    // see: Swift closure expression syntax
+      
+                StarWarsModel.getAllPeople(completionHandler: { // passing what becomes "completionHandler" in the 'getAllPeople' function definition in StarWarsModel.swift
                     data, response, error in
-                    // data -> JSON data, response -> headers and other meta-information, error-> if one occurred
-                    // "do-try-catch" blocks execute a try statement and then use the catch statement for errors
-                    do {
-                        // try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
-                        if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
-                                        if let results = jsonResult["results"] {
-                                            // coercing the results object as an NSArray and then storing that in resultsArray
-                                            let resultsArray = results as! NSArray
-                                            // now we can run NSArray methods like count and firstObject
-                                            for result in resultsArray {
-                                                if let data = result as? NSDictionary {
-                                                    if let result = data["name"] {
-                                                        let name = result as! String
-                                                        self.peopleList.append(name)
-                                                    }
-                                                }
-                                            }
-                                            DispatchQueue.main.async {
-                                                self.pTable.reloadData()
-                                            }
-                                        }
+                        do {
+                            // Try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
+                            if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                                if let results = jsonResult["results"] as? NSArray {
+                                    for person in results {
+                                        let personDict = person as! NSDictionary
+                                        self.peopleList.append(personDict["name"]! as! String)
                                     }
-                                } catch {
-                                    print(error)
                                 }
-                            })
+                            }
+                            DispatchQueue.main.async {
+                                self.pTable.reloadData()
+                            }
+                        } catch {
+                            print("Something went wrong")
+                        }
+                })
+            }
+
                         // execute the task and then wait for the response
                 // to run the completion handler. This is async!
-                task.resume()
+               // task.resume()
         }
     
-    }
+    
 
 
 extension GetPeopleViewController: UITableViewDataSource {
